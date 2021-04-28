40e189fe14ccf3de29429edd31ced66f1a983537
sandygudie <goodnewssandy@gmail.com>
2021-04-28 11:59:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import logo from "./images/logo (2).svg";
+import mockup from "./images/illustration-mockups.svg"
+
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+
+      <header>
+
+        <img src={logo} alt="logo"/>
       </header>
-    </div>
+
+      <main>
+
+        <img src={mockup} alt="mockup" />
+        <div className="content">
+          <h2>Build the community your fans will love</h2>
+          <p>There are also designs for the mobile view and active states for certain elements on the webpage. Those should be used 
+            in the implementation of the design as well.</p>
+            <button> Register</button>
+        </div>
+      </main>
+
+     <footer>
+     <i class="fab fa-facebook-f"></i>
+     </footer>
+     
+    </>
   );
 }
 
